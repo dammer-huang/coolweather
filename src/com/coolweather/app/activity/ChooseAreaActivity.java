6962c72ea98fb69479571edd242648fff3b3dd5d
Dammer <huangjiaamann@163.com>
2015-06-15 23:10:19 +0800

    加入显示天气信息功能。

--- src/com/coolweather/app/activity/ChooseAreaActivity.java
+++ src/com/coolweather/app/activity/ChooseAreaActivity.java
@@ -14,7 +14,10 @@ import com.coolweather.app.util.Utility;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
@@ -80,6 +83,16 @@ public class ChooseAreaActivity extends Activity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		
+		SharedPreferences prefs=PreferenceManager.getDefaultSharedPreferences(this);
+		
+		if(prefs.getBoolean("city_selected", false))
+		{
+			Intent intent=new Intent(this,WeatherActivity.class);
+			startActivity(intent);
+			finish();
+			return;
+		}
+		
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		
 		setContentView(R.layout.choose_area);
@@ -109,6 +122,14 @@ public class ChooseAreaActivity extends Activity {
 				{
 					selectedCity=cityList.get(index);
 					queryCounties();
+				}
+				else if(currentLevel==LEVEL_COUNTY)
+				{
+					String countyCode=countyList.get(index).getCountyCode();
+					Intent intent=new Intent(ChooseAreaActivity.this,WeatherActivity.class);
+					intent.putExtra("county_code", countyCode);
+					startActivity(intent);
+					finish();
 				}
 			}
 		});
